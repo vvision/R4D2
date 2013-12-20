be4f5fa61a00dfd018075a11bad52be80c1a78b1
vvision <vvision@sfr.fr>
2013-12-20 14:52:49 +0100

    Updated mk with bootanimation.

--- tower/R4D2.mk
+++ tower/R4D2.mk
@@ -2,7 +2,16 @@ $(call inherit-product, device/linaro/pandaboard/pandaboard.mk
 
 PRODUCT_PROPERTY_OVERRIDES := ro.hw=lo52
 DEVICE_PACKAGE_OVERLAY:=device/linaro/R4D2/overlay
-PRODUCT_PACKAGES += libusb-shared libusb-static libbmp-shared libbmp-static libbmpExample libbmpExample_jni
+PRODUCT_PACKAGES += \
+  libusb-shared \
+  libusb-static \
+  libbmp-shared \
+  libbmp-static \
+  libbmpExample \
+  libbmpExample_jni
+
+PRODUCT_COPY_FILES += \
+  system/media/bootanimation.zip:system/media/bootanimation.zip
 
 PRODUCT_NAME := R4D2
 PRODUCT_DEVICE := R4D2
